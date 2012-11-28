9beac5f9ba1deff9858b55952953ab7e8a02a677
Patrick Coffey <patrickcoffey48@gmail.com>
2012-11-27 21:03:23 -0600

    Fixing active menu item error

--- submodules/BaseBuildingBlocks_menu/templates/standard_menu.tpl.php
+++ submodules/BaseBuildingBlocks_menu/templates/standard_menu.tpl.php
@@ -1,7 +1,8 @@
 <ul class="nav nav-pills">
   <?php foreach($menu as $menu_item): ?>
+    <?php $active = BaseBuildingBlocks_menu_is_active($menu_item['link']['link_path']) ? 'active': ''; ?>
     <?php if ($menu_item['below']): ?>
-      <li class="dropdown <?php echo BaseBuildingBlocks_menu_is_active($menu_item['link']['link_path']) ? 'active': ''; ?>">
+      <li class="dropdown <?php echo $active; ?>">
         <?php echo l(t('!icon '.$menu_item['link']['link_title'].' !caret', array('!caret' => '<b class="caret"></b>', '!icon' => BaseBuildingBlocks_menu_get_link_icon($menu_item['link']['link_title'], $block_name))), $menu_item['link']['link_path'], 
           array(
             'html' => TRUE,
@@ -19,7 +20,7 @@
         </ul>
       </li>
     <?php else: ?>
-      <li class="<?php echo BaseBuildingBlocks_menu_is_active($menu_item['link']['link_path']) ? 'active': ''; ?>">
+      <li class="<?php echo $active; ?>">
         <?php echo l(t('!icon '.$menu_item['link']['link_title'], array('!icon' => BaseBuildingBlocks_menu_get_link_icon($menu_item['link']['link_title'], $block_name))), $menu_item['link']['link_path'], 
          array(
           'html' => TRUE,
