f94a344488ccc72fac94ec09004d0fc551fedd91
Patrick Coffey <pcoffey@fahrenheitmarketing.com>
2013-04-03 11:57:23 -0500

    Changing name to Bootstrap Blocks

--- submodules/BootstrapBlocks_menu/templates/standard_menu.tpl.php
+++ submodules/BootstrapBlocks_menu/templates/standard_menu.tpl.php
@@ -1,9 +1,9 @@
 <ul class="nav nav-pills">
   <?php foreach($menu as $menu_item): ?>
-    <?php $active = BaseBuildingBlocks_menu_is_active($menu_item['link']['link_path']) ? 'active': ''; ?>
+    <?php $active = BootstrapBlocks_menu_is_active($menu_item['link']['link_path']) ? 'active': ''; ?>
     <?php if ($menu_item['below']): ?>
       <li class="dropdown <?php echo $active; ?>">
-        <?php echo l(t('!icon '.$menu_item['link']['link_title'].' !caret', array('!caret' => '<b class="caret"></b>', '!icon' => BaseBuildingBlocks_menu_get_link_icon($menu_item['link']['link_title'], $block_name))), $menu_item['link']['link_path'], 
+        <?php echo l(t('!icon '.$menu_item['link']['link_title'].' !caret', array('!caret' => '<b class="caret"></b>', '!icon' => BootstrapBlocks_menu_get_link_icon($menu_item['link']['link_title'], $block_name))), $menu_item['link']['link_path'], 
           array(
             'html' => TRUE,
             'attributes' => array(
@@ -14,14 +14,14 @@
         <ul class="dropdown-menu" role="menu" aria-labelledby="<?php echo drupal_clean_css_identifier($menu_item['link']['link_title']); ?>">
           <?php foreach($menu_item['below'] as $submenu_item): ?>
             <li>
-              <?php echo l(t('!icon '.$submenu_item['link']['link_title'], array('!icon' => BaseBuildingBlocks_menu_get_link_icon($submenu_item['link']['link_title'], $block_name))), $submenu_item['link']['link_path'], array('html' => TRUE)); ?>
+              <?php echo l(t('!icon '.$submenu_item['link']['link_title'], array('!icon' => BootstrapBlocks_menu_get_link_icon($submenu_item['link']['link_title'], $block_name))), $submenu_item['link']['link_path'], array('html' => TRUE)); ?>
             </li>
           <?php endforeach; ?>
         </ul>
       </li>
     <?php else: ?>
       <li class="<?php echo $active; ?>">
-        <?php echo l(t('!icon '.$menu_item['link']['link_title'], array('!icon' => BaseBuildingBlocks_menu_get_link_icon($menu_item['link']['link_title'], $block_name))), $menu_item['link']['link_path'], 
+        <?php echo l(t('!icon '.$menu_item['link']['link_title'], array('!icon' => BootstrapBlocks_menu_get_link_icon($menu_item['link']['link_title'], $block_name))), $menu_item['link']['link_path'], 
          array(
           'html' => TRUE,
           'attributes' => array(
