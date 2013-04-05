286177d104b9b11bf3f13504693954151333068b
Patrick Coffey <pcoffey@fahrenheitmarketing.com>
2013-04-05 11:43:39 -0500

    Removing nav pills from navbar, fixing localization issue

--- submodules/BootstrapBlocks_navbar/templates/navbar.tpl.php
+++ submodules/BootstrapBlocks_navbar/templates/navbar.tpl.php
@@ -3,7 +3,7 @@
     <div class="container">
       <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><i class="icon-reorder"></i></a>
       <div class="nav-collapse">
-        <ul class="nav nav-pills">
+        <ul class="nav">
           <?php foreach($menu as $menu_item): ?>
             <?php echo BootstrapBlocks_print_menu_items($menu_item, $block_name, 1); ?>
           <?php endforeach; ?>
