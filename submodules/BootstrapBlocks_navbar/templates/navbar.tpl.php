ae30e6fad5e4744823600c8c37de043865af8e9a
Patrick Coffey <pcoffey@fahrenheitmarketing.com>
2013-04-05 11:39:48 -0500

    Fixing dropdown-toggle issue and adding second level submenus to navbar

--- submodules/BootstrapBlocks_navbar/templates/navbar.tpl.php
+++ submodules/BootstrapBlocks_navbar/templates/navbar.tpl.php
@@ -3,40 +3,9 @@
     <div class="container">
       <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><i class="icon-reorder"></i></a>
       <div class="nav-collapse">
-        <ul class="nav ">
+        <ul class="nav nav-pills">
           <?php foreach($menu as $menu_item): ?>
-            <?php $active = BootstrapBlocks_menu_is_active($menu_item['link']['link_path']) ? 'active': ''; ?>
-            <?php if ($menu_item['below']): ?>
-              <li class="dropdown <?php echo $active; ?>">
-                <?php echo l(t('!icon '.$menu_item['link']['link_title'].' !caret', array('!caret' => '<b class="caret"></b>', '!icon' => BootstrapBlocks_menu_get_link_icon($menu_item['link']['link_title'], $block_name))), $menu_item['link']['link_path'], 
-                  array(
-                    'html' => TRUE,
-                    'attributes' => array(
-                      'class' => array('dropdown-toggle'),
-                      'data-toggle' => 'dropdown',
-                    ),
-                  )); ?>
-                <ul class="dropdown-menu" role="menu" aria-labelledby="<?php echo drupal_clean_css_identifier($menu_item['link']['link_title']); ?>">
-                  <?php foreach($menu_item['below'] as $submenu_item): ?>
-                    <li>
-                      <?php echo l(t('!icon '.$submenu_item['link']['link_title'], array('!icon' => BootstrapBlocks_menu_get_link_icon($submenu_item['link']['link_title'], $block_name))), $submenu_item['link']['link_path'], array('html' => TRUE)); ?>
-                    </li>
-                  <?php endforeach; ?>
-                </ul>
-              </li>
-            <?php else: ?>
-              <li class="<?php echo $active; ?>">
-                <?php echo l(t('!icon '.$menu_item['link']['link_title'], array('!icon' => BootstrapBlocks_menu_get_link_icon($menu_item['link']['link_title'], $block_name))), $menu_item['link']['link_path'], 
-                 array(
-                  'html' => TRUE,
-                  'attributes' => array(
-                    'class' => array(
-                      $active  
-                    ),
-                  ),
-                 )); ?>
-              </li>
-            <?php endif; ?>
+            <?php echo BootstrapBlocks_print_menu_items($menu_item, $block_name, 1); ?>
           <?php endforeach; ?>
         </ul>
       </div>
