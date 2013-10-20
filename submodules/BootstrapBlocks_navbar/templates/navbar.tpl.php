ccba9b5ee79a5a474bfc88652c8bf67836080c87
Patrick Coffey <patrickcoffey48@gmail.com>
2013-10-20 12:08:13 -0500

    Bootstrap 3 update

--- submodules/BootstrapBlocks_navbar/templates/navbar.tpl.php
+++ submodules/BootstrapBlocks_navbar/templates/navbar.tpl.php
@@ -1,14 +1,20 @@
 <div class="navbar <?php echo isset($navbar_classes) ? $navbar_classes : ''; ?>">
-  <div class="navbar-inner">
-    <div class="container">
-      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><i class="icon-reorder"></i></a>
-      <div class="nav-collapse">
-        <ul class="nav">
-          <?php foreach($menu as $menu_item): ?>
-            <?php echo BootstrapBlocks_print_menu_items($menu_item, $block_name, 1); ?>
-          <?php endforeach; ?>
-        </ul>
-      </div>
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#<?php echo $block_name; ?>">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <div class="collapse navbar-collapse" id="<?php echo $block_name; ?>">
+      <ul class="nav navbar-nav">
+        <?php foreach($menu as $menu_item): ?>
+          <?php echo BootstrapBlocks_print_menu_items($menu_item, $block_name, 1); ?>
+        <?php endforeach; ?>
+      </ul>
     </div>
   </div>
 </div>
